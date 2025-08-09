08fc8e9f787c4c5e22ee2cb51f0725bfbe20a467
Vishal <vishalgupta2@gmail.com>
2025-08-10 01:14:28 +0530

    8_9_4

---
+++ app/screens/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+
+export default function ScreensLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
+
